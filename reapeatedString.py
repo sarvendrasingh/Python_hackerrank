4b18a4e35b07a7033201988d085776ee365cd830
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-05-07 20:42:37 +0530

    repeated letter count

---
+++ reapeatedString.py
@@ -0,0 +1,14 @@
+s = input()
+n = int(input().strip())
+count = 0
+for i in s:
+    if i == 'a':
+        count = count + 1
+r = len(s)
+mod = n % r
+times = n // r
+count = times * count
+for i in range(mod):
+    if s[i] == 'a':
+        count = count + 1
+print(count)
