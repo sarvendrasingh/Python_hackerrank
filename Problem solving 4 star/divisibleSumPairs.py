ba3a07b4ef4ffcbc0a01b445814c342dae230091
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-05-23 10:24:12 +0530

    divisible sum pairs from an array counting

---
+++ Problem solving 4 star/divisibleSumPairs.py
@@ -0,0 +1,10 @@
+first_multiple_input = input("Enter values of n and k seperated by space: ").rstrip().split()
+n = int(first_multiple_input[0])
+k = int(first_multiple_input[1])
+ar = list(map(int, input("Enter array of length "+str(n)+": ").rstrip().split()))
+count = 0
+for i in range(n):
+    for j in range(i+1,n):
+        if (ar[i]+ar[j])%k == 0:
+            count += 1
+print(count)
