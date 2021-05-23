6527617b57ab8f5d633f035b220d9605dedf11ac
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-05-23 10:48:19 +0530

    reformat

--- Problem solving 4 star/migratoryBirds.py
+++ Problem solving 4 star/migratoryBirds.py
@@ -1,7 +1,7 @@
 arr_count = int(input("Enter number of birds: ").strip())
 arr = list(map(int, input("Enter type of each bird seperated by space: ").rstrip().split()))
-arr_freq = [0,0,0,0,0]
+arr_freq = [0, 0, 0, 0, 0]
 for i in arr:
-    arr_freq[i-1] += 1
-ans = arr_freq.index(max(arr_freq))+1
-print("Maximum frequency birds was of type: "+str(ans))
+    arr_freq[i - 1] += 1
+ans = arr_freq.index(max(arr_freq)) + 1
+print("Maximum frequency birds was of type: " + str(ans))
